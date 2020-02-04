ffe9b2b8fdbe4e27d50b12dfd6c79b9bab98d679
”名前” <>
2020-02-04 12:48:41 +0000

    Rails 05 ログイン機能の実装の課題も修正しました。ログイン後出ないと投稿ボタンが押せないように制御しました。

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/
+  validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX}
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  
+  validates :name, presence: true, length: { maximum: 15}
+
+  has_secure_password
+  
+  has_many :topics
+end
